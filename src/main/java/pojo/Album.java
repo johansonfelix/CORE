cb4f7d446115b41d2633fd04e55535c257da7387
johansonfelix <48577361+johansonfelix@users.noreply.github.com>
2021-02-05 09:59:17 -0500

    Test

---
+++ src/main/java/pojo/Album.java
@@ -0,0 +1,70 @@
+package pojo;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "album")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Album {
+    @XmlElement
+    private String ISRC;
+    @XmlElement
+    private String title;
+    @XmlElement
+    private String description;
+    @XmlElement
+    private String releaseYear;
+    @XmlElement
+    private String artist;
+    public Album(){
+
+    }
+    public String getISRC() {
+        return ISRC;
+    }
+
+    public void setISRC(String ISRC) {
+        this.ISRC = ISRC;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(String releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String nickname) {
+        this.artist = nickname;
+    }
+    public String toString(){
+        return "ISRC: " + ISRC +", " +
+                "Title: " + title + ", " +
+                "Description: " + description + ", " +
+                "Release year: " + releaseYear + ", " +
+                "Artist: " + artist;
+    }
+}
---
+++ src/main/java/pojo/Artist.java
@@ -0,0 +1,66 @@
+package pojo;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "artist")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Artist {
+    @XmlElement
+    private String firstName;
+    @XmlElement
+    private String lastName;
+    @XmlElement
+    private String nickName;
+    @XmlElement
+    private String bio;
+    public Artist(){
+
+    }
+    public Artist(String firstName, String lastName, String nickName, String bio) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nickName = nickName;
+        this.bio = bio;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+    public String toString() {
+        return "First name: " + firstName + ", " +
+                "Last name: " + lastName + ", " +
+                " Nickname: " + nickName + ", Bio: " + bio + ".";
+
+    }
+}
